1307a5102fd7e3e0aeb3a72ae396cec1919a7cf2
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 00:40:29 -0300

    feat: testando smptmailService abstract

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
 import { SMTPMailService } from './mail/smtp.mail.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [SMTPMailService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: SMTPMailService,
+    },
+  ],
 })
 export class AppModule {}
