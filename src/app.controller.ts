07d6efdff1a8d9e2a2f03bdf59ce3ae745aa4906
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 01:13:12 -0300

    feat: criando método de criar notificação

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller('app')
+@Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
-  @Get('hello')
-  getHello() {
+  @Get()
+  list() {
     return this.prisma.notification.findMany(); //connectando à tablea
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
