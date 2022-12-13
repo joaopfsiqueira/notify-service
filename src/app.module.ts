8e0a9e0c136899af937bdc9b0122f2eea620e35e
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 00:43:23 -0300

    feat: um modulo pode chamar o outro

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
 import { MailService } from './mail/mail.service';
 import { PostmarkMailService } from './mail/postmark.mail.service';
 import { SMTPMailService } from './mail/smtp.mail.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [
     {
