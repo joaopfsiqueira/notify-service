8231b05385db1ef0c3694a25d9a329eb992d543d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 01:29:39 -0300

    feat: usando validation pipe para pegar o erro

--- src/create-notification-body.ts
+++ src/create-notification-body.ts
@@ -1,5 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateNotificationBody {
+  @IsNotEmpty() //dizendo que recipient id não pode estar vazio, tratando o erro caso ele venha vazio
   recipientId: string;
+
   content: string;
   category: string;
 }
