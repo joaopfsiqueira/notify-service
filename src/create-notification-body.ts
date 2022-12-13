44dbbf415357926c59c87243dd57f853c7ca1d32
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 01:34:55 -0300

    feat: usando class-validator

--- src/create-notification-body.ts
+++ src/create-notification-body.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
   @IsNotEmpty() //dizendo que recipient id não pode estar vazio, tratando o erro caso ele venha vazio
+  @IsUUID() // dizendo que precisa ser um UUID
   recipientId: string;
 
+  @IsNotEmpty() //dizendo que recipient id não pode estar vazio, tratando o erro caso ele venha vazio
+  @Length(5, 240) //definindo limite minimo e maximo.
   content: string;
+
   category: string;
 }
