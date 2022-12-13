1cd1a295daa25ed912941e3c45da7a60e86e340e
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 00:41:49 -0300

    feat: tirando as rotas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MailService } from './mail/mail.service';
 import { SMTPMailService } from './mail/smtp.mail.service';
 
