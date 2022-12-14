303c4626deb6c3101b6e2f713f399b0addb03df2
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-14 01:13:34 -0300

    feat: mudando para injectable

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { Notification } from '../../../../app/entities/notification';
 import { NotificationsRepository } from '../../../../app/repositories/notifications-repository';
 import { PrismaService } from '../prisma.service';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
