c8b0894968df5c24cc4f790d92941312de57d691
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-14 01:06:14 -0300

    feat: atualizando controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { PrismaService } from '../../database/prisma/prisma.service';
+import { SendNotification } from 'src/app/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 
 @Controller('notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     //é dessa forma que pegamos o req.body (express) usando nestjs. Ou melhor, o corpo da requisição. @Body() nomevariavel: tipo
     console.log(body);
     const { recipientId, content, category } = body;
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
   }
 }
