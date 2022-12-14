baa259fdfcc19365f99793aa52adc60083d0b644
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-14 01:09:22 -0300

    feat: devolvendo prisma quando for só notifications

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/app/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
   imports: [],
   controllers: [NotificationsController],
+  //tudo o que tiver de providers é injeção de dependências.
+  providers: [SendNotification],
 })
 export class HttpModule {}
