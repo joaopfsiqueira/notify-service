9bd54f80632d1682fc35a3174c65c56ab480700d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 22:45:17 -0300

    fix: limpando código

--- src/app/entities/notification.spec.ts
+++ src/app/entities/notification.spec.ts
@@ -11,11 +11,4 @@ describe('Notifcation', () => {
 
     expect(notification).toBeTruthy();
   });
-  it('should not be able to create a notification content with less than 5 characters', () => {
-    expect(() => new Content('aaa')).toThrow();
-  });
-
-  it('should not be able to create a notification content with more than 240 characters', () => {
-    expect(() => new Content('a'.repeat(241))).toThrow(); //.repeat é um método js que pega a string e repete ela pelo numero de vezes.
-  });
 });
