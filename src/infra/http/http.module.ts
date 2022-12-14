67a6b96fd1230a465d1572ffc7f1c47ce60a4239
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-14 01:10:28 -0300

    feat: importante database module dentro do http

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SendNotification } from 'src/app/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [NotificationsController],
   //tudo o que tiver de providers é injeção de dependências.
   providers: [SendNotification],
