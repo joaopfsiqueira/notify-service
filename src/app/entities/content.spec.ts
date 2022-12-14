28cf4a84a5b32adb74ccf6429bb83dc49aed5151
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-13 22:30:09 -0300

    feat: melhorando código de teste

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,14 +1,16 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('Você recebeu uma solicitação de amizade');
+describe('Notifcation content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Você recebeu uma solicitação de amizade');
 
-  expect(content).toBeTruthy();
-});
-test('it should not be able to create a notification content with less than 5 characters', () => {
-  expect(() => new Content('aaa')).toThrow();
-});
+    expect(content).toBeTruthy();
+  });
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('aaa')).toThrow();
+  });
 
-test('it should not be able to create a notification content with more than 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow(); //.repeat é um método js que pega a string e repete ela pelo numero de vezes.
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow(); //.repeat é um método js que pega a string e repete ela pelo numero de vezes.
+  });
 });
